b82b7ad3702fb3abed03d9780d945d31bc10ed5f
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-07-01 16:04:11 +0100

    Simplified tests.

--- tests.py
+++ tests.py
@@ -1,22 +1,43 @@
+import sys
+if sys.version_info[0] < 3:
+    raise ImportError("These tests require Python 3 to run.")
+
 import os
 import shutil
 import subprocess
-import sys
 import tempfile
 import unittest
 import winreg
 
+
 SCRIPT_TEMPLATE='''%(shebang_line)s%(coding_line)simport sys
 print(sys.version)
 print(sys.argv)
-'''
+%(comment)s'''
 
 BOM_UTF8 = b'\xEF\xBB\xBF'
-BOM_UTF16LE = b'\xFF\xFE'
-BOM_UTF16BE = b'\xFE\xFF'
-LAUNCHER = os.path.join('Release', 'py.exe')
+
+LAUNCHER = os.path.join('Debug', 'py.exe')
 
 IS_W = sys.executable.endswith("w.exe")
+
+SHEBANGS = {
+    'SHEBANG_NONE': '',
+    'SHEBANG_ENV_PY': '#!/usr/bin/env python\n',
+    'SHEBANG_ENV_PY2': '#!/usr/bin/env python2\n',
+    'SHEBANG_ENV_PY3': '#!/usr/bin/env python3\n',
+    'SHEBANG_BIN_PY': '#!/usr/bin/python\n',
+    'SHEBANG_BIN_PY2': '#!/usr/bin/python2\n',
+    'SHEBANG_BIN_PY3': '#!/usr/bin/python3\n',
+    'SHEBANG_LBIN_PY': '#!/usr/local/bin/python\n',
+    'SHEBANG_LBIN_PY2': '#!/usr/local/bin/python2\n',
+    'SHEBANG_LBIN_PY3': '#!/usr/local/bin/python3\n',
+    'SHEBANG_PY': '#!python\n',
+    'SHEBANG_PY2': '#!python2\n',
+    'SHEBANG_PY3': '#!python3\n',
+}
+
+COMMENT_WITH_UNICODE = '# Libert\xe9, \xe9galit\xe9, fraternit\xe9\n'
 
 VIRT_PATHS = [
     "/usr/bin/env ",
@@ -116,22 +137,47 @@ class ScriptLaunchTest(unittest.TestCase):
         shutil.rmtree(self.work_dir)
 
     def make_script(self, shebang_line='', coding_line='', encoding='ascii',
-                    bom=b''):
-        script = (SCRIPT_TEMPLATE % locals()).encode(encoding)
-        script = script.replace(b'\r', b'').replace(b'\n', b'\r\n')
+                    bom=b'', comment=''):
+        script = (SCRIPT_TEMPLATE % locals())
+        script = script.replace('\r', '').replace('\n',
+                                                  '\r\n').encode(encoding)
+        if bom and not script.startswith(bom):
+            script = bom + script
         path = os.path.join(self.work_dir, 'showver.py')
         with open(path, 'wb') as f:
-            f.write(bom + script)
+            f.write(script)
+        self.last_script = script
         return path
 
     def matches(self, stdout, pyinfo):
-        return stdout.startswith(pyinfo.bversion)
+        result = stdout.startswith(pyinfo.bversion)
+        if not result:
+            with open('last_failed.py', 'wb') as f:
+                f.write(self.last_script)
+            print(pyinfo.bversion)
+            for s in self.last_streams:
+                print(repr(s))
+        return result
+
+    def is_encoding_error(self, message):
+        return b'but no encoding declared; see' in message
 
     def run_child(self, path):
         p = subprocess.Popen([LAUNCHER, path], stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE, shell=False)
         stdout, stderr = p.communicate()
+        self.last_streams = stdout, stderr
         return stdout, stderr
+
+    def get_python_for_shebang(self, shebang):
+        if '3' in shebang:
+            result = DEFAULT_PYTHON3
+        else:
+            result = DEFAULT_PYTHON2
+        return result
+
+    def get_coding_line(self, coding):
+        return '# -*- coding: %s -*-\n' % coding
 
     def test_help(self):
         p = subprocess.Popen([LAUNCHER, '-h'], stdout=subprocess.PIPE,
@@ -140,100 +186,54 @@ class ScriptLaunchTest(unittest.TestCase):
         self.assertTrue(stdout.startswith(b'Python Launcher for Windows'))
         self.assertIn(b'The following help text is from Python:\r\n\r\nusage: ', stdout)
 
-    def test_no_shebang_ascii(self):
-        path = self.make_script()
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
+    # Tests with ASCII Python sources
+    def test_shebang_ascii(self):
+        for shebang in SHEBANGS.values():
+            path = self.make_script(shebang_line=shebang)
+            stdout, stderr = self.run_child(path)
+            python = self.get_python_for_shebang(shebang)
+            self.assertTrue(self.matches(stdout, python))
 
-    def test_py_shebang_ascii(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python\n')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
+    # Tests with UTF-8 Python sources with no BOM
+    def test_shebang_utf8_nobom(self):
+        for shebang in SHEBANGS.values():
+            # If there's no Unicode, all should be well
+            path = self.make_script(shebang_line=shebang, encoding='utf-8')
+            stdout, stderr = self.run_child(path)
+            python = self.get_python_for_shebang(shebang)
+            self.assertTrue(self.matches(stdout, python))
+            # If there's a Unicode comment with no coding line to alert,
+            # we should see those errors from the spawned Python
+            path = self.make_script(shebang_line=shebang, encoding='utf-8',
+                                    comment=COMMENT_WITH_UNICODE)
+            stdout, stderr = self.run_child(path)
+            # Python3 reads Unicode without BOM as UTF-8
+            self.assertTrue(self.is_encoding_error(stderr) or '3' in shebang)
+            path = self.make_script(shebang_line=shebang, encoding='utf-8',
+                                    comment=COMMENT_WITH_UNICODE,
+                                    coding_line=self.get_coding_line('utf-8'))
+            stdout, stderr = self.run_child(path)
+            self.assertTrue(self.matches(stdout, python))
 
-    def test_py2_shebang_ascii(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python2\n')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
+    # Tests with UTF-8 Python sources with BOM
+    def test_shebang_utf8_bom(self):
+        for shebang in SHEBANGS.values():
+            # If there's no Unicode, all should be well
+            path = self.make_script(shebang_line=shebang, encoding='utf-8',
+                                    bom=BOM_UTF8)
+            stdout, stderr = self.run_child(path)
+            python = self.get_python_for_shebang(shebang)
+            self.assertTrue(self.matches(stdout, python))
+            # If there's a Unicode comment, we should still be fine as
+            # there's a BOM
+            path = self.make_script(shebang_line=shebang, encoding='utf-8',
+                                    comment=COMMENT_WITH_UNICODE,
+                                    bom=BOM_UTF8)
+            stdout, stderr = self.run_child(path)
+            python = self.get_python_for_shebang(shebang)
+            self.assertTrue(self.matches(stdout, python))
 
-    def test_py3_shebang_ascii(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python3\n')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON3))
 
-    def test_no_shebang_utf8_nobom(self):
-        path = self.make_script(encoding='utf-8')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py_shebang_utf8_nobom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python\n',
-                                encoding='utf-8')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py2_shebang_utf8_nobom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python2\n',
-                                encoding='utf-8')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py3_shebang_utf8_nobom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python3\n',
-                                encoding='utf-8')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON3))
-
-    def test_no_shebang_utf8_bom(self):
-        path = self.make_script(encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py_shebang_utf8_bom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py2_shebang_utf8_bom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python2\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py3_shebang_utf8_bom(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python3\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON3))
-
-    def test_no_shebang_utf8_bom_coding(self):
-        path = self.make_script(encoding='utf-8', bom=BOM_UTF8,
-                                coding_line='# -*- coding: utf-8 -*-\n')
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py_shebang_utf8_bom_coding(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python\n',
-                                coding_line='# -*- coding: utf-8 -*-\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py2_shebang_utf8_bom_coding(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python2\n',
-                                coding_line='# -*- coding: utf-8 -*-\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON2))
-
-    def test_py3_shebang_utf8_bom_coding(self):
-        path = self.make_script(shebang_line='#!/usr/bin/env python3\n',
-                                coding_line='# -*- coding: utf-8 -*-\n',
-                                encoding='utf-8', bom=BOM_UTF8)
-        stdout, stderr = self.run_child(path)
-        self.assertTrue(self.matches(stdout, DEFAULT_PYTHON3))
-
-        
 if __name__ == '__main__':
     unittest.main()
 

d6a3b8e0e018140940d55a6f878e2a9eff13464e
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-07-05 19:58:55 +0100

    Updated to reference UI extension.

--- buildmsi.cmd
+++ buildmsi.cmd
@@ -3,12 +3,12 @@ candle -d"Manufacturer=Vinay Sajip" -d"Platform=x86" -d"Version=1.0.0.0" launch_
 light launch_module.wixobj
 del launch_module.wixobj
 candle -d"Manufacturer=Vinay Sajip" -d"Platform=x86" -d"Version=1.0.0.0" launcher.wxs
-light launcher.wixobj
+light -ext WixUIExtension launcher.wixobj
 del launcher.wixobj
 
 candle -d"Manufacturer=Vinay Sajip" -d"Platform=x64" -d"Version=1.0.0.0" launch_module.wxs
 light -o launch_module.amd64.msm launch_module.wixobj
 del launch_module.wixobj
 candle -d"Manufacturer=Vinay Sajip" -d"Platform=x64" -d"Version=1.0.0.0" launcher.wxs
-light -o launcher.amd64.msi launcher.wixobj
+light -o launcher.amd64.msi -ext WixUIExtension launcher.wixobj
 del launcher.wixobj

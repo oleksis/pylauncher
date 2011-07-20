344d61b2d76d086ca8143e113d339733298593dc
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-07-20 16:27:12 +0100

    Changed Python version used to build MSIs.

--- buildmsi.cmd
+++ buildmsi.cmd
@@ -1,10 +1,10 @@
 @echo off
 REM x86 ...
-\python27\python makemsi.py -o launchsys Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
-\python27\python makemsi.py -o launchwin Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
-\python27\python makemsi.py Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher
+\python32\python makemsi.py -o launchsys Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
+\python32\python makemsi.py -o launchwin Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
+\python32\python makemsi.py Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher
 
 REM x64 ...
-\python27\python makemsi.py -o launchsys Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
-\python27\python makemsi.py -o launchwin Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
-\python27\python makemsi.py Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher
+\python32\python makemsi.py -o launchsys Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
+\python32\python makemsi.py -o launchwin Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
+\python32\python makemsi.py Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher

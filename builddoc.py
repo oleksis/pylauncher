8444d786efe7603bde206085625b125fa0b7f92d
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-07-21 11:00:34 +0100

    Documentation build changes.

---
+++ builddoc.py
@@ -0,0 +1,28 @@
+#!python3.2
+try:
+    from docutils.core import publish_cmdline
+except ImportError:
+    publish_cmdline = None
+import os.path
+import sys
+
+def main():
+    if publish_cmdline is None:
+        sys.stderr.write('Unable to produce documentation: docutils not found.\n')
+        rc = 1
+    else:
+        infile = os.path.join('Doc', 'launcher.rst')
+        outfile = os.path.join('Doc', 'launcher.html')
+        try:
+            publish_cmdline(writer_name='html',
+                            argv=[infile, outfile])
+            rc = 0
+        except Exception:
+            e = sys.exc_info()[1]
+            sys.stderr.write('Failed when producing documentation: %s\n',
+                             e)
+            rc = 2
+    return rc
+
+if __name__ == '__main__':
+    sys.exit(main())
--- buildmsi.cmd
+++ buildmsi.cmd
@@ -1,10 +1,11 @@
 @echo off
+\python32\python builddoc.py
 REM x86 ...
-\python32\python makemsi.py -o launchsys Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
+REM \python32\python makemsi.py -o launchsys Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
 \python32\python makemsi.py -o launchwin Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
 \python32\python makemsi.py Platform=x86 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher
 
 REM x64 ...
-\python32\python makemsi.py -o launchsys Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
+REM \python32\python makemsi.py -o launchsys Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToSystem launcher
 \python32\python makemsi.py -o launchwin Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" ToWindows launcher
 \python32\python makemsi.py Platform=x64 Version=1.0.0.0 "Manufacturer=Vinay Sajip" launcher

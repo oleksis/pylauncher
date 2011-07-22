fb3d8918663f46711ae150d5ffe465f89bb920ce
Mark Hammond <mhammond@skippinet.com.au>
2011-07-22 11:10:51 +1000

    don't overwrite existing ini file during build step

--- CLILauncher/copy_config.cmd
+++ CLILauncher/copy_config.cmd
@@ -1,4 +1,4 @@
 @echo off
 Echo copying .ini files ...
-copy global.ini %1py.ini
-copy local.ini %appdata%\py.ini
+if not exist "%1py.ini" copy global.ini "%1py.ini"
+if not exist "%appdata%\py.ini" copy local.ini "%appdata%\py.ini"

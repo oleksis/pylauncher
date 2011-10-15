5171b1d32382d3f60a70716d16a678c791e50e16
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2011-10-15 14:05:33 +0100

    Added buildmsi.py.

---
+++ buildmsi.py
@@ -0,0 +1,23 @@
+import getpass
+import os
+import sys
+
+VERSION = 'Version=1.0.0.2'
+MANUFACTURER = 'Manufacturer=Vinay Sajip'
+X86 = 'Platform=x86'
+X64 = 'Platform=x64'
+TOWIN = 'ToWindows'
+
+def main():
+    signpwd = getpass.getpass('Password for signing:')
+    import builddoc
+    builddoc.main()
+    os.environ['SIGNPWD'] = signpwd
+    import makemsi
+    makemsi.main(['-o', 'launchwin', X86, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main([X86, VERSION, MANUFACTURER, 'launcher'])
+    makemsi.main(['-o', 'launchwin', X64, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main([X64, VERSION, MANUFACTURER, 'launcher'])
+
+if __name__ == '__main__':
+    sys.exit(main())

011d4b6b31d401f75d0866052a78eb65488b36c0
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2015-04-26 10:12:23 +0100

    Updated to use version number in .msi filenames.

--- buildmsi.py
+++ buildmsi.py
@@ -2,7 +2,8 @@ import getpass
 import os
 import sys
 
-VERSION = 'Version=1.0.1.6'
+VER = '1.0.1.6'
+VERSION = 'Version=%s' % VER
 MANUFACTURER = 'Manufacturer=Vinay Sajip'
 X86 = 'Platform=x86'
 X64 = 'Platform=x64'
@@ -14,10 +15,10 @@ def main():
     builddoc.main()
     os.environ['SIGNPWD'] = signpwd
     import makemsi
-    makemsi.main(['-o', 'launchwin', X86, VERSION, MANUFACTURER, TOWIN, 'launcher'])
-    makemsi.main([X86, VERSION, MANUFACTURER, 'launcher'])
-    makemsi.main(['-o', 'launchwin', X64, VERSION, MANUFACTURER, TOWIN, 'launcher'])
-    makemsi.main([X64, VERSION, MANUFACTURER, 'launcher'])
+    makemsi.main(['-o', 'launchwin-%s' % VER, X86, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main(['-o', 'launcher-%s' % VER, X86, VERSION, MANUFACTURER, 'launcher'])
+    makemsi.main(['-o', 'launchwin-%s' % VER, X64, VERSION, MANUFACTURER, TOWIN, 'launcher'])
+    makemsi.main(['-o', 'launcher-%s' % VER, X64, VERSION, MANUFACTURER, 'launcher'])
 
 if __name__ == '__main__':
     sys.exit(main())
